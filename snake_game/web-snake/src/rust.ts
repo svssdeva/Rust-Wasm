7d780dc4db1bb0e72b25f162c9df841114b165c3
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-05-11 13:02:46 +0530

    feat: workaround added to serve wasm files

--- snake_game/web-snake/src/rust.ts
+++ snake_game/web-snake/src/rust.ts
@@ -1,9 +1,37 @@
-async function init() {
-    const byteArray = new Int8Array([0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01, 0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07, 0x07, 0x01, 0x03, 0x73, 0x75, 0x6d, 0x00, 0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b, 0x00, 0x18, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x01, 0x06, 0x01, 0x00, 0x03, 0x73, 0x75, 0x6d, 0x02, 0x09, 0x01, 0x00, 0x02, 0x00, 0x01, 0x61, 0x01, 0x01, 0x62]);
-    const wasm = await WebAssembly.instantiate(byteArray.buffer);
-    const sumFunction = wasm.instance.exports.sum;
-    //@ts-ignore
-    const result = sumFunction(100, 1000);
-    console.log(result);
+async function init(): Promise<void> {
+    let result = 0;
+    try {
+        const response = await fetch("http://localhost:3000/sum");
+        const buffer = await response.arrayBuffer();
+        console.log(buffer);
+        result = await add(buffer);
+    } catch (error) {
+        console.error(error);
+        // ... (handle error case)
+    } finally {
+        console.log(result);
+    }
 }
 init();
+init();
+anotherAdd();
+async function add(buffer: ArrayBuffer): Promise<number> {
+    const { instance } = await WebAssembly.instantiate(buffer);
+    const sumFunction = instance.exports.sum as (a: number, b: number) => number;
+    return sumFunction(100, 1000);
+}
+
+async function anotherAdd() {
+    try {
+        const response = await fetch("http://localhost:3000/sum");
+        console.log(response.headers.get('Content-Type')); // Add this line
+        const module = await WebAssembly.compileStreaming(response);
+        const instance = await WebAssembly.instantiate(module);
+        const sumFunction = instance.exports.sum;
+        //@ts-ignore
+        const result = sumFunction(100, 1000);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
