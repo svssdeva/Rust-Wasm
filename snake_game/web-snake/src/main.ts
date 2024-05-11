6374c4f7c084940209b5402573aadd283f6486de
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-05-11 11:10:57 +0530

    feat: Vite flow verification with wasm

--- snake_game/web-snake/src/main.ts
+++ snake_game/web-snake/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -13,12 +13,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
